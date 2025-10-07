2be2bf47541d30dbd8b114cbfae3ef891430c7ae
Tatiana Muromtseva <tamuromt@yandex.ru>
2025-10-08 01:46:26 +0300

    Corrected two_segments_of_integers.c file: variables corrected for camelCase format

--- src/two_segments_of_integers.c
+++ src/two_segments_of_integers.c
@@ -1,46 +1,46 @@
 #include <stdio.h>
 
-void reverse_segment(int *array, int start_index, int end_index) {
-    while (start_index < end_index) {
-        int temp = array[start_index];
-        array[start_index] = array[end_index];
-        array[end_index] = temp;
-        start_index++;
-        end_index--;
+void reverse_segment(int *array, int startIndex, int endIndex) {
+    while (startIndex < endIndex) {
+        int temp = array[startIndex];
+        array[startIndex] = array[endIndex];
+        array[endIndex] = temp;
+        startIndex++;
+        endIndex--;
     }
 }
 
-void swap_two_segments(int *array, int first_segment_length, int second_segment_length) {
-    int total_length = first_segment_length + second_segment_length;
-    reverse_segment(array, 0, first_segment_length - 1);
-    reverse_segment(array, first_segment_length, total_length - 1);
-    reverse_segment(array, 0, total_length - 1);
+void swap_two_segments(int *array, int firstSegmentLength, int secondSegmentLength) {
+    int totalLength = firstSegmentLength + secondSegmentLength;
+    reverse_segment(array, 0, firstSegmentLength - 1);
+    reverse_segment(array, firstSegmentLength, totalLength - 1);
+    reverse_segment(array, 0, totalLength - 1);
 }
 
 int main() {
-    int first_segment_length = 0;
-    int second_segment_length = 0;
+    int firstSegmentLength = 0;
+    int secondSegmentLength = 0;
 
     printf("Введите длину первого сегмента массива (m): ");
-    scanf("%d", &first_segment_length);
+    scanf("%d", &firstSegmentLength);
 
     printf("Введите длину второго сегмента массива (n): ");
-    scanf("%d", &second_segment_length);
+    scanf("%d", &secondSegmentLength);
 
-    int total_length = first_segment_length + second_segment_length;
-    int input_array[total_length];
+    int totalLength = firstSegmentLength + secondSegmentLength;
+    int inputArray[totalLength];
 
-    printf("Введите %d целых чисел массива через пробел:\n", total_length);
-    for (int i = 0; i < total_length; ++i) {
-        input_array[i] = 0;
-        scanf("%d", &input_array[i]);
+    printf("Введите %d целых чисел массива через пробел:\n", totalLength);
+    for (int i = 0; i < totalLength; ++i) {
+        inputArray[i] = 0;
+        scanf("%d", &inputArray[i]);
     }
 
-    swap_two_segments(input_array, first_segment_length, second_segment_length);
+    swap_two_segments(inputArray, firstSegmentLength, secondSegmentLength);
 
     printf("Полученный массив:\n");
-    for (int i = 0; i < total_length; ++i) {
-        printf("%d ", input_array[i]);
+    for (int i = 0; i < totalLength; ++i) {
+        printf("%d ", inputArray[i]);
     }
     printf("\n");
 
