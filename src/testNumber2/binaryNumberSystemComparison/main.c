64a4fdd53ae630c5dfa77e4449654d95e218505d
Tatiana Muromtseva <tamuromt@yandex.ru>
2025-12-01 15:08:57 +0300

    decided to add some comments in main.c

--- src/testNumber2/binaryNumberSystemComparison/main.c
+++ src/testNumber2/binaryNumberSystemComparison/main.c
@@ -5,22 +5,29 @@
 int main() {
   int lenA, lenB;
 
+  // Ввод длины массива A
   scanf("%d", &lenA);
+
   bool a[lenA];
+  // тут чтение битов массива A
   for (int i = 0; i < lenA; i++) {
     int bit;
-    scanf("%d", &bit);
-    a[i] = bit ? true : false;
+    scanf("%d", &bit);         // ввод бита (0 или 1)
+    a[i] = bit ? true : false; // преобразование в булев тип
   }
 
+  // Ввод длины массива B
   scanf("%d", &lenB);
+
   bool b[lenB];
+  // тут чтение битов массива B
   for (int i = 0; i < lenB; i++) {
     int bit;
-    scanf("%d", &bit);
-    b[i] = bit ? true : false;
+    scanf("%d", &bit);         // ввод бита (0 или 1)
+    b[i] = bit ? true : false; // преобразование в булев тип
   }
 
+  // вызоваем функцию чтобы сравнить
   int result = compareBinary(a, lenA, b, lenB);
 
   if (result > 0) {
