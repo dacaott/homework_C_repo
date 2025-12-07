afb71ab85445c42335a3a2ca46279cd29c1d28a2
Tatiana Muromtseva <tamuromt@yandex.ru>
2025-12-08 00:57:47 +0300

    Fixing files for Sorted List (clang format)

--- src/homeworkNumber6/theSortedList/sortedList.h
+++ src/homeworkNumber6/theSortedList/sortedList.h
@@ -2,13 +2,13 @@
 #define SORTEDLIST_H
 
 typedef struct Node {
-  int value;
-  struct Node *next;
+    int value;
+    struct Node* next;
 } Node;
 
-void add_sorted(Node **head, int val);
-void delete_value(Node **head, int val);
-void print_list(Node *head);
-void free_list(Node *head);
+void add_sorted(Node** head, int val);
+void delete_value(Node** head, int val);
+void print_list(Node* head);
+void free_list(Node* head);
 
 #endif
