bcbfb884e285fdfee07998be2d48c8696f1a5b19
Tatiana Muromtseva <tamuromt@yandex.ru>
2025-12-20 23:59:40 +0300

    add four files (full homework)

---
+++ src/homeworkNumber2/twoSegmentsOfIntegers.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void reverseSegment(int* array, int startIndex, int endIndex)
+{
+    while (startIndex < endIndex) {
+        int temp = array[startIndex];
+        array[startIndex] = array[endIndex];
+        array[endIndex] = temp;
+        startIndex++;
+        endIndex--;
+    }
+}
+
+void swapTwoSegments(int* array, int firstSegmentLength,
+    int secondSegmentLength)
+{
+    int totalLength = firstSegmentLength + secondSegmentLength;
+    reverseSegment(array, 0, firstSegmentLength - 1);
+    reverseSegment(array, firstSegmentLength, totalLength - 1);
+    reverseSegment(array, 0, totalLength - 1);
+}
+
+int main()
+{
+    int firstSegmentLength = 0;
+    int secondSegmentLength = 0;
+
+    printf("Введите длину первого сегмента массива (m): ");
+    scanf("%d", &firstSegmentLength);
+
+    printf("Введите длину второго сегмента массива (n): ");
+    scanf("%d", &secondSegmentLength);
+
+    int totalLength = firstSegmentLength + secondSegmentLength;
+    int inputArray[totalLength];
+
+    printf("Введите %d целых чисел массива через пробел:\n", totalLength);
+    for (int i = 0; i < totalLength; ++i) {
+        inputArray[i] = 0;
+        scanf("%d", &inputArray[i]);
+    }
+
+    swapTwoSegments(inputArray, firstSegmentLength, secondSegmentLength);
+
+    printf("Полученный массив:\n");
+
+    for (int i = 0; i < totalLength; ++i) {
+        printf("%d ", inputArray[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
