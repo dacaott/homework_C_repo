a09679f60456aa1f1aa599597f060d51ddaf9d69
Tatiana Muromtseva <tamuromt@yandex.ru>
2025-12-21 15:24:38 +0300

    remake lucky tickets

--- src/homeworkNumber2/luckyTickets.c
+++ src/homeworkNumber2/luckyTickets.c
@@ -3,17 +3,7 @@
 int main()
 {
     int tripletCount[28] = { 0 }; // количество троек цифр с каждой суммой (0…27)
-    int sum = 0;
-    int triples = 0;
     int luckyTickets = 0;
-
-    printf("Введите сумму цифр (от 0 до 27): ");
-    scanf("%d", &sum);
-
-    if (sum < 0 || sum > 27) {
-        printf("Некорректный ввод.\n");
-        return 1;
-    }
 
     for (int first = 0; first <= 9; first++) {
         for (int second = 0; second <= 9; second++) {
@@ -24,11 +14,11 @@ int main()
         }
     }
 
-    triples = tripletCount[sum];
-
-    luckyTickets = triples * triples;
-
-    printf("Количество счастливых билетов = %d\n", luckyTickets);
+    // суммируем квадраты
+    for (int s = 0; s <= 27; s++) {
+        luckyTickets += tripletCount[s] * tripletCount[s];
+    }
+    printf("Общее количество счастливых билетов = %d\n", luckyTickets);
 
     return 0;
 }
